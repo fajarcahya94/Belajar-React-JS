879238dfe056e51752c237b201d573f94c0dba35
Fajar Cahya <fajarcahya94@gmail.com>
2022-11-26 16:08:00 +0800

    secon commit

---
+++ src/components/Herosection.js
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+
+const Herosection = () => {
+  return (
+    <div>
+      <Container className="d-flex align-items-center justify-content-center text-center bg">
+        <h1>Ayo Nobar Film!</h1>
+      </Container>
+    </div>
+  );
+};
+
+export default Herosection;
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,38 @@
+import { Button, Container, Form, Nav, Navbar, Offcanvas } from "react-bootstrap";
+import icon1 from "../../src/assets/icons/icon1.png";
+
+const NavigationBar = () => {
+  return (
+    <div>
+      {["lg"].map((expand) => (
+        <Navbar key={expand} expand={expand}>
+          <Container>
+            <Navbar.Brand href="#home">
+              {/* <a href="https://www.flaticon.com/free-icons/film" title="film icons">Film icons created by BZZRINCANTATION - Flaticon</a> */}
+              <img alt="icon" src={icon1} width="30" height="30" className="d-inline-block align-items-center" /> Movies
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas id={`offcanvasNavbar-expand-${expand}`} aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`} placement="end">
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>Movies</Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-center flex-grow-1 pe-3">
+                  <Nav.Link href="#action1">Home</Nav.Link>
+                  <Nav.Link href="#action2">Tentang Kami</Nav.Link>
+                  <Nav.Link href="#action2">Film</Nav.Link>
+                </Nav>
+                <Form className="d-flex">
+                  <Form.Control type="search" placeholder="Cari Film" className="me-2" aria-label="Search" />
+                  <Button>Search</Button>
+                </Form>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </div>
+  );
+};
+
+export default NavigationBar;
